d5f1e7baf71f6b62c1fdf020623210a85ec28f40
hamin971231 <hamin971231@gmail.com>
2023-05-22 17:18:19 +0900

    Day1

---
+++ calculators.py
@@ -0,0 +1,23 @@
+a=3
+b=4
+# 덧셈 : +, 빼기 : -, 곱하기: *, 나누기 : /, 몫 : //, 나머지 : %
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+# 자료의 형변환 
+# str(숫자)-> 문자, int(실수) -> 정수 
+
+a=10
+b=20
+# 결과값이 1020이 나오도록 덧셈을 하여라
+c=int(str(a)+str(b))
+print(c)
+
+# d의 소수부분을 잘라라 
+d=2333.223165
+print(int(d))
