19301c83ca768569fecf18c7086b5f7bd14abd0f
hamin971231 <hamin971231@gmail.com>
2023-05-23 10:20:56 +0900

    Day2

--- calculators.py
+++ calculators.py
@@ -9,15 +9,5 @@ print(a/b)
 print(a//b)
 print(a%b)
 
-# 자료의 형변환 
-# str(숫자)-> 문자, int(실수) -> 정수 
 
-a=10
-b=20
-# 결과값이 1020이 나오도록 덧셈을 하여라
-c=int(str(a)+str(b))
-print(c)
 
-# d의 소수부분을 잘라라 
-d=2333.22316543542
-print(int(d))
