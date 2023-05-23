687ed13a96ec04c5582722e2345bca2ef2814776
hamin971231 <hamin971231@gmail.com>
2023-05-23 13:05:29 +0900

    test

--- calculators.py
+++ calculators.py
@@ -2,12 +2,25 @@ a=3
 b=4
 # 덧셈 : +, 빼기 : -, 곱하기: *, 나누기 : /, 몫 : //, 나머지 : %
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
-print(a%b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+# 제곱, 제롭근
+# 2의 10제곱을 출력하라 
+import math
+print(2**10)
+print(math.pow(2,10)) 
+
+# 1024의 제곱근을 구하라 
+# 제곱근은 math라는 라이브러리를 import 해줘야한다. 
+
+print(math.sqrt(1024))
+
+
 
 
 
